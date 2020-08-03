713995502fa6d75a1956cd97476d8b4513fb3e7d
Stas Kolkovsky <stas.kolkovsky@gmail.com>
2020-08-04 00:57:50 +0300

    Fixing routing in auth module

--- src/app/modules/dashboard/modules/trash/components/list-items/list-items.component.ts
+++ src/app/modules/dashboard/modules/trash/components/list-items/list-items.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list-items',
-  templateUrl: './list-items.component.html',
-  styleUrls: ['./list-items.component.css']
+  templateUrl: './list-items.component.html'
 })
 export class ListItemsComponent implements OnInit {
 
