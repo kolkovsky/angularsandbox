9fcd4b00fb005ace9d09bdf625637da16052057b
Stas Kolkovsky <stas.kolkovsky@gmail.com>
2020-09-20 19:52:40 +0300

    Add mixin for responsive layout implementation

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -1,15 +1,21 @@
-import { Injectable } from "@angular/core";
-import { Observable, Subject, BehaviorSubject } from "rxjs";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, Subject, BehaviorSubject, of } from 'rxjs';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class AuthService {
+  private API_URL: string = 'Ff';
   private successUserLogin$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
-  constructor() {}
+  constructor(private httpCLient: HttpClient) {}
 
   public isUserLogged(): Observable<boolean> {
     return this.successUserLogin$;
+  }
+
+  public login(login): Observable<void> {
+    return of(null);
   }
 }
